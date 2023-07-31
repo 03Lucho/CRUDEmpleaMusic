11b80fce734d7c17ed3d5db3dcb04330f2fc44e5
esteban <estebonemoreno2002@gmail.com>
2023-07-31 16:24:58 -0500

    Agregando carpeta app de controladores y modelos empleamusic ESTEBAN 1 'App'

---
+++ app/Http/Controllers/AprendizController.php
@@ -0,0 +1,93 @@
+<?php
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Models\Aprendiz;
+
+class AprendizController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $aprendices =Aprendiz::orderBy('nombre','DESC')->get();
+        return view('aprendiz/index',['aprendices'=>$aprendices]);
+    }
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        return view('aprendiz.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $data = $request->validate([
+            'nombre' => 'required|max:50',
+            'apellido' => 'required|max:50',
+            'email' => 'required|email|unique:aprendices',
+            'contrasena' => 'required|min:6|max:15',
+            'telefono' => 'required|max:25',
+            'descripcion' => 'required|max:700',
+        ]);
+
+        Aprendiz::create($data);
+
+        return redirect()->route('aprendiz.index')->with('success', 'Aprendiz creado exitosamente.');
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Aprendiz $aprendiz)
+    {
+        return view('aprendiz.show', compact('aprendiz'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Aprendiz $aprendiz)
+    {
+        return view('aprendiz.edit', compact('aprendiz'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Aprendiz $aprendiz)
+    {
+        $data = $request->validate([
+            'nombre' => 'required|max:50',
+            'apellido' => 'required|max:50',
+            'email' => 'required|email|unique:aprendices,email,' . $aprendiz->idaprendiz,
+            'contrasena' => 'nullable|min:6|max:15',
+            'telefono' => 'required|max:25',
+            'descripcion' => 'required|max:700',
+        ]);
+
+        if ($request->has('contrasena')) {
+            $data['contrasena'] = bcrypt($request->contrasena);
+        }
+
+        $aprendiz->update($data);
+
+        return redirect()->route('aprendiz.index')->with('success', 'Aprendiz actualizado exitosamente.');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Aprendiz $aprendiz)
+    {
+        $aprendiz->delete();
+
+        return redirect()->route('aprendiz.index')->with('success', 'Aprendiz eliminado exitosamente.');
+    }
+}
---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use Illuminate\Support\Facades\Auth;
+
+use App\Models\Aprendiz;
+
+class AuthController extends Controller
+{
+    public function showLoginForm()
+    {
+        return view('auth.login');
+    }
+
+    public function login(Request $request)
+    {
+        $credentials = $request->validate([
+            'email' => 'required|email',
+            'password' => 'required',
+        ]);
+
+        if (Auth::attempt($credentials)) {
+            $user = Auth::user();
+            if ($user->rol === 'administrador') {
+                return redirect()->route('admin.dashboard');
+            } elseif ($user->rol === 'profesor') {
+                return redirect()->route('profesor.dashboard');
+            } else {
+                return redirect()->route('aprendiz.dashboard');
+            }
+        } else {
+            return back()->withErrors(['email' => 'Credenciales incorrectas']);
+        }
+    }
+
+    public function logout()
+    {
+        Auth::logout();
+        return redirect('/login');
+    }
+}
+
